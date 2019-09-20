3c97820c7d0c010d8ce0a837d130fe631480ca6b
Alexander Nagy <zsombor86@gmail.com>
2019-09-20 15:16:24 +0200

    Create simple-port-scanner.py

---
+++ simple-port-scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket()
+
+rhosts = ['127.0.0.1'] # 
+rports = [22, 445, 80, 443, 3389]
+
+for host in rhosts:
+    for port in rports:
+      try:
+          print "[+] Connecting to "+host+":"+str(port)
+          s.connect((host, port))
+          s.send('simple port scanner')
+          banner = s.recv(1024)
+          if banner:
+            print("[+] Port "+str(port)+" is open: "+banner)
+          s.close()
+      except:pass
