8a8ee99dc5793ae45799698114690d213fe7f8b5
Alexander Nagy <zsombor86@gmail.com>
2019-09-20 15:17:36 +0200

    Update simple-port-scanner.py

--- simple-port-scanner.py
+++ simple-port-scanner.py
@@ -2,8 +2,8 @@ import socket
 
 s = socket.socket()
 
-rhosts = ['127.0.0.1'] # 
-rports = [22, 445, 80, 443, 3389]
+rhosts = ['127.0.0.1'] # add hosts to the scope
+rports = [22, 445, 80, 443, 3389] # add ports to the scope
 
 for host in rhosts:
     for port in rports:
